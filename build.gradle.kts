528cb2c4f87b09f1a229a338061832e2a040435f
scaramanga <scaramangado@gmail.com>
2019-09-11 20:22:29 +0200

    Add Lily dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,22 @@
+plugins {
+  java
+  kotlin("jvm") version "1.3.50"
+}
+
 group = "ootbingo.barinade"
 version = "0.0.1-SNAPSHOT"
 
+repositories {
+  mavenCentral()
+  jcenter()
+  maven {
+    url = uri("http://h2841273.stratoserver.net:10091/repository/maven-releases/")
+  }
+}
+
+dependencies {
+  implementation("de.scaramanga:lily:0.1.0")
+}
 
 tasks.withType(Wrapper::class) {
   gradleVersion = "5.6.2"
