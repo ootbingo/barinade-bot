9befacbd1827ab78bc13eb873b5c8578ece6eb5c
scaramanga <scaramangado@gmail.com>
2019-11-01 13:26:06 +0100

    Add Bingo filter

--- src/main/kotlin/ootbingo/barinade/bot/model/Race.kt
+++ src/main/kotlin/ootbingo/barinade/bot/model/Race.kt
@@ -1,5 +1,35 @@
 package ootbingo.barinade.bot.model
 
+import ootbingo.barinade.bot.properties.BingoRaceProperties
+import java.time.ZoneId
 import java.time.ZonedDateTime
 
-data class Race(val srlId: String, val goal:String, val recordDate: ZonedDateTime, val numberOfEntrants: Long, val raceResults: List<RaceResult>)
+data class Race(val srlId: String, val goal: String, val recordDate: ZonedDateTime, val numberOfEntrants: Long,
+                val raceResults: List<RaceResult>)
+
+fun Race.isBingo(): Boolean {
+
+  val blacklistedWords = listOf("short", "long", "blackout", "black out", "3x3", "anti", "double", "bufferless",
+                                "child", "jp", "japanese", "bingo-j")
+
+  if (this.srlId.toInt() in BingoRaceProperties.blacklist) {
+    return false
+  }
+
+  with(this.goal.toLowerCase()) {
+    return when {
+
+      containsAny(blacklistedWords) -> false
+
+      contains("speedrunslive.com/tools/oot-bingo") ->
+        this@isBingo.recordDate.isBefore(ZonedDateTime.of(2019, 9, 21, 0, 0, 0, 0, ZoneId.of("UTC")))
+      matches(Regex("https?://ootbingo\\.github\\.io/bingo/v\\d+\\.\\d/bingo\\.html.*")) -> true
+
+      else -> false
+    }
+  }
+}
+
+private fun String.containsAny(patterns: Collection<String>): Boolean {
+  return patterns.any { it in this }
+}
