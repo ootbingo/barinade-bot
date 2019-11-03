c0c241e52570123408165a4c2f9850385c9572fb
scaramanga <scaramangado@gmail.com>
2019-11-03 17:03:04 +0100

    Enable test report

--- Jenkinsfile
+++ Jenkinsfile
@@ -17,6 +17,7 @@ pipeline {
         stage("Test") {
             steps {
                 sh "./gradlew test"
+                step([$class: 'JUnitResultArchiver', testResults: 'build/test-results/test/*.xml' ])
             }
         }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
   id("org.springframework.boot") version "2.1.8.RELEASE"
   kotlin("jvm") version "1.3.50"
   kotlin("plugin.spring") version "1.3.50"
+  jacoco
 }
 
 group = "ootbingo.barinade"
@@ -56,6 +57,12 @@ tasks.withType(Jar::class) {
 
 tasks.withType<Test>() {
   useJUnitPlatform()
+
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+
+  finalizedBy("jacocoTestReport")
 }
 
 val compileKotlin: KotlinCompile by tasks
